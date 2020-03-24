3ae34c16ec8cf5ab207afc45bb44ec97821085dd
Frolov <f-miralius@yandex.ru>
2020-03-24 12:33:12 +0400

    Finished first lab with error

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -4,6 +4,7 @@ import numpy
 from sympy import symbols
 from sympy import S
 from sympy import solveset
+from sympy import diff
 
 x = symbols('x')
 
@@ -58,6 +59,39 @@ def find_solve_with_secant_method(func, a, b, eps):
         else:
             a = c
             f_a = f_c
+    print("МЕТОД ПОЛОВИННОГО ДЕЛЕНИЯ")
+    print("Найденный корень x* = " + str(root))
+    print("Невязка f(x*) = " + str(func.subs(x, root)))
+    print("Кол-во итераций = " + str(count))
+    print("Абсолютная погрешность = " + str(abs(root - exact_solution)))
+    print("Относительная погрешность = " + str(abs(root - exact_solution) / abs(root) * 100) + "%")
+
+
+def find_solve_with_hybrid_method(func, a, b, x0, eps):
+    x_k = x0
+    count = 0
+    exact_solution = 0
+    i = 0
+    solutions = solveset(func)
+    while i < len(solutions):
+        if a < solutions.args[i] < b:
+            exact_solution = solutions.args[i]
+        i += 1
+    while True:
+        x_k1 = x0 - (func.subs(x, x_k)) / ((diff(func, x)).subs(x, x_k))
+        while True:
+            count += 1
+            if abs(func.subs(x, x_k1)) < abs(func.subs(x, x_k)):
+                root = x_k1
+                x_k = (x_k1 + x_k) / 2
+                break
+            else:
+                x_k_temp = x_k
+                x_k = x_k1
+                x_k1 = (x_k1 + x_k_temp) / 2
+        if abs(root - x_k) <= eps:
+            break
+    print("ГИБРИДНЫЙ МЕТОД")
     print("Найденный корень x* = " + str(root))
     print("Невязка f(x*) = " + str(func.subs(x, root)))
     print("Кол-во итераций = " + str(count))
@@ -68,6 +102,9 @@ def find_solve_with_secant_method(func, a, b, eps):
 plot_function(x ** 3 + 8.5 * x ** 2 + 21.8 * x + S(15.6), -10, 10)
 plot_function(x ** 3 + 8.5 * x ** 2 + 21.8 * x + S(15.6), -8, 0)
 plot_function(x ** 3 + 8.5 * x ** 2 + 21.8 * x + S(15.6), -4.8, 0)
-find_solve_with_secant_method(x ** 3 + 8.5 * x ** 2 + 21.8 * x + S(15.6), -4.8, -3.84, 10**(-3))
-find_solve_with_secant_method(x ** 3 + 8.5 * x ** 2 + 21.8 * x + S(15.6), -3.84, -2.88, 10**(-3))
-find_solve_with_secant_method(x ** 3 + 8.5 * x ** 2 + 21.8 * x + S(15.6), -1.92, -0.96, 10**(-3))
+find_solve_with_secant_method(x ** 3 + 8.5 * x ** 2 + 21.8 * x + S(15.6), -4.8, -3.84, 10 ** (-3))
+find_solve_with_secant_method(x ** 3 + 8.5 * x ** 2 + 21.8 * x + S(15.6), -3.84, -2.88, 10 ** (-3))
+find_solve_with_secant_method(x ** 3 + 8.5 * x ** 2 + 21.8 * x + S(15.6), -1.92, -0.96, 10 ** (-3))
+find_solve_with_hybrid_method(x ** 3 + 8.5 * x ** 2 + 21.8 * x + S(15.6), -4.8, -3.84, -4, 10 ** (-3))
+find_solve_with_hybrid_method(x ** 3 + 8.5 * x ** 2 + 21.8 * x + S(15.6), -3.84, -2.88, -3, 10 ** (-3))
+find_solve_with_hybrid_method(x ** 3 + 8.5 * x ** 2 + 21.8 * x + S(15.6), -1.92, -0.96, -1, 10 ** (-3))
