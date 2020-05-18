4490070a509738443f28f194fbd5daf4cc966cec
Frolov <f-miralius@yandex.ru>
2020-05-18 23:04:27 +0400

    Есть функция табуляции f(x) на [a, b]

--- Lab2/Lab3.py
+++ Lab2/Lab3.py
@@ -6,8 +6,18 @@ from sympy import symbols, pi, diff, N
 x = symbols('x')
 
 
+def function(point):
+    return numpy.math.pi * point / (10 + point / 2)
+
+
 def define_step(func):
     return N(2 * ((eps / abs(diff(func, x, 4)).subs(x, a)) ** (1 / 4)))
+
+
+# noinspection SpellCheckingInspection
+def discretize():
+    n = numpy.ceil((b - a) / h)
+    return numpy.array([a + i * h for i in range(n)]), numpy.array([function(a + i * h) for i in range(n)])
 
 
 if __name__ == "__main__":
@@ -20,3 +30,5 @@ if __name__ == "__main__":
     print("Точность = " + str(eps))
     h = define_step(pi * x / (10 + x / 2))
     print("Шаг = " + str(h))
+    x, y = discretize()
+    print("x = " + str(x))
