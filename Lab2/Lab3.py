7fea44ad41d6dfb3ba198afbd44f1b017e4ef726
Frolov <f-miralius@yandex.ru>
2020-05-18 23:14:22 +0400

    Есть построение интерполяционного полинома Лагранжа (не доделан)

--- Lab2/Lab3.py
+++ Lab2/Lab3.py
@@ -14,10 +14,24 @@ def define_step(func):
     return N(2 * ((eps / abs(diff(func, x, 4)).subs(x, a)) ** (1 / 4)))
 
 
-# noinspection SpellCheckingInspection
 def discretize():
     n = numpy.ceil((b - a) / h)
     return numpy.array([a + i * h for i in range(n)]), numpy.array([function(a + i * h) for i in range(n)])
+
+
+def interpolate_with_the_Lagrange_polynomial(x, otr):
+    i = 0
+    L = 0
+    while i < 4:
+        j = 0
+        mult = 1
+    while j < 4:
+        if i != j:
+            mult *= (x - otr[j]) / (otr[i] - otr[j])
+        j += 1
+        # L += f(otr[i]) * mult
+        i += 1
+    return L
 
 
 if __name__ == "__main__":
