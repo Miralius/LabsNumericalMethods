bbaf7f1364202e27a607efc05bb7eba1b6045c4d
Frolov <f-miralius@yandex.ru>
2020-05-18 19:58:43 +0400

    Заготовка второй лабы

---
+++ Lab2/Lab3.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot
+import matplotlib.ticker
+import numpy
+from sympy import symbols, S, sqrt, solveset, diff
+
+if __name__ == "__main__":
+    print("Лабораторная работа №3")
