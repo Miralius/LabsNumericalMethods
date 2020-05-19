fa012499ca6acdbbb3025ed7e7175400c73a092e
Frolov <f-miralius@yandex.ru>
2020-05-19 12:44:14 +0400

    Навел порядок в первом задании, окончательно приступаю ко второму

--- Lab2/Lab3.py
+++ Lab2/Lab3.py
@@ -14,8 +14,8 @@ def define_step(func):
     return N(2 * ((eps / (abs(diff(func, x, 4)).subs(x, a))) ** (1 / 4)))
 
 
-def discretize(left, right, h):
-    n = round((right - left) / h)
+def discretize():
+    n = round((b - a) / h)
     return n, numpy.array([a + i * h for i in range(n)]), numpy.array([function(a + i * h) for i in range(n)])
 
 
@@ -34,25 +34,29 @@ def interpolate_with_the_lagrange_polynomial(point, interval):
     return interpolated_function
 
 
-def plot_function(x, func):
+def interpolate_with_the_newton_method(x, x_L, y_L):
+    return x_L
+
+
+def plot_function(x, func, name):
     xy = matplotlib.pyplot.subplot()
     xy.grid(which='major', color='k')
     xy.minorticks_on()
     xy.grid(which="minor", color='gray', linestyle=':')
-    xy.plot(x, func, color="blue", label="f1(x)")
+    xy.plot(x, func, color="blue", label=name)
     xy.set_xlabel("x")
     xy.set_ylabel("y")
     xy.legend()
     matplotlib.pyplot.show()
 
 
-def plot_functions(ox, func1, func2):
+def plot_functions(x, func1, func2, name1, name2):
     xy = matplotlib.pyplot.subplot()
     xy.grid(which='major', color='k')
     xy.minorticks_on()
     xy.grid(which="minor", color='gray', linestyle=':')
-    xy.plot(ox, func1, color="blue", label="f1(x)")
-    xy.plot(ox, func2, color="red", label="f2(x)")
+    xy.plot(x, func1, color="blue", label=name1)
+    xy.plot(x, func2, color="red", label=name2)
     xy.set_xlabel("x")
     xy.set_ylabel("y")
     xy.legend()
@@ -67,13 +71,23 @@ if __name__ == "__main__":
     print("a = " + str(a))
     print("b = " + str(b))
     print("Точность = " + str(eps))
-    step = define_step(pi * x / (10 + x / 2))
-    print("Шаг = " + str(step))
-    number, x, y = discretize(a, b, step)
-    plot_function(x, y)
+
+    print("Первое задание ---------")
+    h = define_step(pi * x / (10 + x / 2))
+    print("Шаг = " + str(h))
+    number, x, y = discretize()
+    plot_function(x, y, "f(x) = pi * x / (10 + x / 2)")
     print("x = " + str(x))
     number_x = int(input("Выберите начало отрезка x для интерполяции полиномами Лагранжа: "))
-    number_L, x_L, y_L = discretize(x[number_x], x[number_x + 3], (x[number_x + 3] - x[number_x]) / number)
-    y_interpolated_by_lagrange = numpy.array([interpolate_with_the_lagrange_polynomial(x_L[j], numpy.array([x[number_x + i] for i in range(4)])) for j in range(len(x_L))])
-    plot_functions(x_L, y_L, y_interpolated_by_lagrange)
-    plot_function(x_L, numpy.array([abs(y_L[i] - y_interpolated_by_lagrange[i]) for i in range(len(x_L))]))
+    x_L = numpy.array([x[number_x + i] for i in range(4)])
+    y_interpolated_by_lagrange = numpy.array([interpolate_with_the_lagrange_polynomial(x_L[number_x + i], x_L) for i in range(len(x_L))])
+    a = x_L[0]
+    b = x_L[3]
+    h = (x_L[3] - x_L[0]) / number
+    number, x, y = discretize()
+    y_L = numpy.array([interpolate_with_the_lagrange_polynomial(x[i], x_L) for i in range(len(x))])
+    plot_functions(x, y, y_L, "f(x)", "interpolated function by Lagrange polynomial")
+    plot_function(x, numpy.array([abs(y_L[i] - y[i]) for i in range(len(x))]), "absolute error")
+
+    print("Второе задание ---------")
+    y_interpolated_by_newton = numpy.array([interpolate_with_the_newton_method(x_L[i], y_interpolated_by_lagrange[i]) for i in range(len(x_L))])
